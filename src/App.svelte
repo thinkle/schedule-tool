84b8ac9a67ba42be50a1b25a9ba1c2ae4022b0a8
Tom Hinkle <tmhinkle@gmail.com>
2021-11-15 18:56:20 -0500

    ++About

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,33 @@
   import Schedule from "./Schedule.svelte";
   import { schedule } from "./blocks";
   import Collapser from "./Collapser.svelte";
+  let about = false;
 </script>
+
+<a
+  href="#about"
+  on:click={(e) => {
+    about = true;
+    e.preventDefault();
+  }}>About</a
+>
+<a href="/?edit">Start Fresh</a>
+<div class="modal" class:show={about} on:click={() => (about = false)}>
+  <div class="about">
+    <h2>Tom's Schedule Tool</h2>
+    <p>Welcome to my schedule tool.</p>
+    <p>
+      Just copy the URL (Control-L, Control-C) to share your schedule idea width
+      someone.
+    </p>
+    <p>
+      This has handy tools to edit block times, edit block names, and move
+      blocks around. My hope is they are intuitive enough to use without
+      instructions. If I failed on that front, let me know and I'll try to make
+      improvements. Generally, you can click on things and edit them :)
+    </p>
+  </div>
+</div>
 
 <Schedule {schedule} />
 
@@ -18,5 +44,27 @@
     border: none;
     padding: none;
     background-color: #f0f0e0;
+  }
+  .modal {
+    display: none;
+  }
+  .modal.show {
+    position: fixed;
+    z-index: 200;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    overflow-y: auto;
+    background-color: #22222288;
+  }
+  .modal .about {
+    max-width: 800px;
+    background-color: white;
+    padding: 1em;
+    line-height: 1.3;
   }
 </style>
